b5721584aa37eaae90e7fa26cea5633e19fac4c9
Your Name <you@example.com>
2024-02-26 21:54:10 +0530

    added S3 backend

--- providers.tf
+++ providers.tf
@@ -12,3 +12,11 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "b80-terraform-bucket"
+    key    = "tf-current-state-file"
+    region = "us-east-1"
+  }
+}
